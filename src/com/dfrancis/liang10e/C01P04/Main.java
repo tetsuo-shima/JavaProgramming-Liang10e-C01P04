668f5c700c27093936e2c5ce5ffdaff7310101eb
Dwight Francis <dfrancis0@gmail.com>
2014-09-09 12:11:54 -0400

    Initial commit

---
+++ src/com/dfrancis/liang10e/C01P04/Main.java
@@ -0,0 +1,31 @@
+package com.dfrancis.liang10e.C01P04;
+
+/*
+(Print a table) Write a program that displays the following table:
+        a       a^2     a^3
+        1       1       1
+        2       4       8
+        3       9       27
+        4       16      64
+*/
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("a\t\ta^2\t\ta^3");
+
+	    for (int index = 1; index <= 4; index++){
+            System.out.printf("%d\t\t%d\t\t%d\n", index, square(index), cube(index));
+        }
+    }
+
+    public static int square(int number) {
+        return number * number;
+    }
+
+    public static int cube(int number) {
+        return number * number * number;
+    }
+}
+
+
